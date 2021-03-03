09e150edca71e87aff76babb53df9353dfaf852c
Koo-Code <75971277+Koo-Code@users.noreply.github.com>
2021-03-03 23:59:25 +0900

    1-3課題提出

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,25 @@
+//question1
+let a = 10;
+let b = 11
+
+console.log(a !== 10);
+console.log(10 <= b && b < 20 && b%2 === 0);
+
+//question2
+let x = 10;
+
+if (10 <= x && x <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+
+//question3
+let y = 0;
+
+if (y%2 === 0){
+    console.log("偶数です");
+}else{
+    console.log("奇数です");
+}
